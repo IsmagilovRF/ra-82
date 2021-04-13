ec78f429b1a0887b674e72eb2b690b4bdb423330
IsmagilovRF <ledohod@icloud.com>
2021-04-13 16:33:44 +0300

    version 1.0.3

--- src/components/HookJsonFetch.js
+++ src/components/HookJsonFetch.js
@@ -2,12 +2,13 @@ import React from "react";
 import useJsonFetch from "./useJsonFetch";
 
 function HookJsonFetch({ url }) {
-  const [data, loading, error] = useJsonFetch(url);
+  const [data, error, loading] = useJsonFetch(url);
+  console.log('[data, error, loading] = '+ [data, error, loading]);
   return (
     <div className="ui segment">
-      {data && <p>{data.status}</p>}
-      {loading && <p>Loading</p>}
-      {error && <p>Error</p>}
+      {data && <p>'data.status = ' {data.status}</p>}
+      {loading && <p>'Loading = ' Loading</p>}
+      {error && <p>"Error = " Error</p>}
     </div>
   );
 }
--- src/components/useJsonFetch.js
+++ src/components/useJsonFetch.js
@@ -2,8 +2,12 @@ import { useState, useEffect } from "react";
 
 const useJsonFetch = (url, opts) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // console.log('url = ' + url, '  useJsonFetch [data, setData]  = '+ [data, setData] );
+  // console.log('url = ' + url, '  useJsonFetch [error, setError]  = '+ [error, setError] );
+  // console.log('url = ' + url, '  useJsonFetch [loading, setLoading]  = '+ [loading, setLoading] );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,6 +16,9 @@ const useJsonFetch = (url, opts) => {
         const response = await fetch(url);
         if (!response.ok) throw new Error(response.statusText);
         const data = await response.json();
+        // console.log('url = ' + url, '  useJsonFetch data.status = '+ data.status);
+        // console.log('url = ' + url, '  useJsonFetch response.statusText = '+ response.statusText);
+        // console.log('url = ' + url, '  useJsonFetch error = '+ error);
         setData(data);
         setError(null);
       } catch (err) {
@@ -22,7 +29,8 @@ const useJsonFetch = (url, opts) => {
     };
     fetchData();
   }, [url]);
-  return [data, loading, error];
+  // console.log('url = ' + url, '  useJsonFetch error = '+ error);
+  return [data, error, loading];
 };
 
 export default useJsonFetch;
